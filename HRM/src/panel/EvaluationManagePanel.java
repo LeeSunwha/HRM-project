6929c9aa50b9c9d91c5406329544051576cd1dec
user <user@Server03>
2020-02-11 21:32:56 +0900

    Ryu - 버튼추가

--- HRM/src/panel/EvaluationManagePanel.java
+++ HRM/src/panel/EvaluationManagePanel.java
@@ -1,13 +1,22 @@
 package panel;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
- * @brief
- * @author 
- * @version 
+ * @brief 인사평가 등록
+ * @author 유지광
+ * @version v1.00 2020.02.11 
  * @see 
  */
 public class EvaluationManagePanel extends JPanel{
 
+	public EvaluationManagePanel() {
+		// TODO Auto-generated constructor stub
+		JButton btn = new JButton("버튼1");
+		btn.setBounds(10,10,100,30);
+		
+		add(btn);
+		
+	}
 }
