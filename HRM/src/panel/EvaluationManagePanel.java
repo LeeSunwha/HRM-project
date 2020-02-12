fd3bc92975aa0ee2c2fe2233aa8c941f4dfbc618
user <user@Server03>
2020-02-12 20:26:59 +0900

    ryu-0212-1

--- HRM/src/panel/EvaluationManagePanel.java
+++ HRM/src/panel/EvaluationManagePanel.java
@@ -2,6 +2,10 @@ package panel;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+
 
 /**
  * @brief 인사평가 등록
@@ -11,12 +15,37 @@ import javax.swing.JPanel;
  */
 public class EvaluationManagePanel extends JPanel{
 
+	
+	private Frame frame;
+	private Button button1, button2, button3;
+	
+	
 	public EvaluationManagePanel() {
 		// TODO Auto-generated constructor stub
-		JButton btn = new JButton("버튼1");
-		btn.setBounds(10,10,100,30);
 		
-		add(btn);
+		
+		frame = new Frame();
+		button1 = new Button ("버튼1");
+		button2 = new Button ("열기");
+		button3 = new Button ("닫기");
+		
 		
 	}
-}
+	
+	public void startFrame() {
+		
+		frame.setLayout( new FlowLayout());
+		frame.add(button1);
+		frame.add(button2);
+		frame.add(button3);
+		
+		
+		frame.setSize (300, 300);
+		frame.setVisible(true);
+		
+	}
+		
+}		
+		
+	
+		
